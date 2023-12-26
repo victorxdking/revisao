5b52762105c1c67c82e421de270683e31e8ff4c8
victorxdking <victorxdking@gmail.com>
2023-12-26 16:36:14 -0300

    Revisando Objetos

--- aula70.js
+++ aula70.js
@@ -1,4 +1,4 @@
-const pessoa1 = new Object();
+/* const pessoa1 = new Object();
 pessoa1.nome = 'Luiz';
 pessoa1.sobrenome = 'Otávio';
 pessoa1.idade = 26;
@@ -12,4 +12,37 @@ pessoa1.getDataNascimento = function() {
 
 for (let chave in pessoa1) {
     console.log(pessoa1[chave]);
+} */
+
+
+
+
+// Factory functions / Constructor funcitons / Classes
+/* function criaPessoa(nome, sobrenome) {
+    return {
+        nome, 
+        sobrenome,
+        nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        }
+    };
 }
+
+const p1 = criaPessoa('Luiz', 'Otávio');
+console.log(p1.nomeCompleto()); */
+
+
+// Factory functions / Constructor funcitons / Classes
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    Object.freeze(this);
+}
+
+const p1 = new Pessoa('Luiz', 'Miranda');
+p1.nome = 'Outra coisa';
+const p2 = new Pessoa('Maria', 'Miranda');
+
+console.log(p1);
+console.log(p2);
